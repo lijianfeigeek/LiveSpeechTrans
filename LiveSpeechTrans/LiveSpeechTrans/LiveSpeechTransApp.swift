f44bd303d187f726e99bbb2d7c73453cbce80a03
LI JIANFEI <me@lijianfei.com>
2024-12-10 10:04:15 +0800

    初始化

---
+++ LiveSpeechTrans/LiveSpeechTrans/LiveSpeechTransApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveSpeechTransApp.swift
+//  LiveSpeechTrans
+//
+//  Created by LIJIANFEI on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveSpeechTransApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
